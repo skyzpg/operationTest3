5c8c052c492369fc3a4a15a5fe5e679856b154db
tygitt <918342704@qq.com>
2019-11-11 17:28:27 +0800

    push test

---
+++ src/main/java/mypackage/MainApp.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class MainApp {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("hello git");
+    }
+}
