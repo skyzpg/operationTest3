5f3cb26d314445d0381b30b40b2a99ce71803da8
tygitt <918342704@qq.com>
2019-11-11 17:46:00 +0800

    modify

--- src/main/java/mypackage/MainApp.java
+++ src/main/java/mypackage/MainApp.java
@@ -4,5 +4,7 @@ public class MainApp {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("hello git");
+
+        System.out.println("hello hadoop");
     }
 }
